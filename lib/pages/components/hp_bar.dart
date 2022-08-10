95286e7ff6e41bd20e4a321d2e5879a46f91815d
kk_boomer <fking.dmitry@gmail.com>
2022-08-10 19:52:09 +0200

    Создан хп бар

---
+++ lib/pages/components/hp_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HpBarWidget extends StatelessWidget {
+  const HpBarWidget({
+    super.key,
+    required this.isReady,
+    required this.hp,
+  });
+
+  final bool isReady;
+  final double hp;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 100, maxHeight: 10),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.bounceOut,
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: isReady ? Colors.red : Colors.black.withOpacity(0))),
+            height: 10,
+            width: 100,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInCubic,
+              color: Colors.red,
+              height: 10,
+              width: isReady ? hp : 0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,5 @@
-import 'package:easy_animation/pages/components/eyes.dart';
-import 'package:easy_animation/pages/components/mouth.dart';
-import 'package:easy_animation/pages/components/text_and_hair.dart';
+
+import 'package:easy_animation/pages/components/hp_bar.dart';
 import 'package:flutter/material.dart';
 
 import 'components/game_start_button.dart';
@@ -15,21 +14,31 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   //Булевое значения благодаря которому мы будем менять состояние
   bool isReady = false;
+  double hp = 100;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          //Делаем контейнер кликабельным
-          child: GestureDetector(
-        //Обрабатываем нажатие
-        onTap: () => setState(() {
-          //Именяем булевое значение на противоположное себе
-          isReady = !isReady;
-        }),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HpBarWidget(isReady: isReady, hp: hp),
+            const SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              //Обрабатываем нажатие
+              onTap: () => setState(() {
+                //Именяем булевое значение на противоположное себе
+                isReady = !isReady;
+              }),
 
-        child: GameStgitartButton(isReady: isReady),
-      )),
+              child: GameStartButton(isReady: isReady),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
