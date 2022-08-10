1507115a14e0fa04933480b7b2d509135e0a3e7e
kk_boomer <fking.dmitry@gmail.com>
2022-08-10 18:29:21 +0200

    Начальная анимация кнопки

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,17 @@
 import 'package:easy_animation/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
-
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Просто назначил темную тему, мне так нравится
       theme: ThemeData.dark(),
+      //Здесь мы описываем все наши экраны
       routes: {"/main_page": (context) => const MainPage()},
+      //Запускает экран
       initialRoute: "/main_page",
     );
   }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isReady = false;
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+          child: GestureDetector(
+        onTap: () => setState(() {
+          isReady = !isReady;
+        }),
+        child: AnimatedContainer(
+          decoration: BoxDecoration(
+              color: isReady ? Colors.red : Colors.black,
+              borderRadius:
+                  BorderRadius.all(Radius.circular(isReady ? 0 : 50))),
+          width: 100,
+          height: 100,
+          duration: const Duration(milliseconds: 80),
+          curve: Curves.easeOutBack,
+          child: AnimatedAlign(
+              alignment: isReady ? Alignment.topCenter : Alignment.center,
+              duration: const Duration(milliseconds: 150),
+              curve: Curves.easeOutBack,
+              child: Text(isReady ? "||||" : "START")),
+        ),
+      )),
+    );
   }
 }
