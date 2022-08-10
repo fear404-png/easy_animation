0d903ae39e788938dcc0d88c2c0d36a772994774
kk_boomer <fking.dmitry@gmail.com>
2022-08-10 18:40:04 +0200

    Добавлены коментарии в main_page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -8,24 +8,42 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  //Булевое значения благодаря которому мы будем менять состояние
   bool isReady = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+          //Делаем контейнер кликабельным
           child: GestureDetector(
+        //Обрабатываем нажатие
         onTap: () => setState(() {
+          //Именяем булевое значение на противоположное себе
           isReady = !isReady;
         }),
+
         child: AnimatedContainer(
           decoration: BoxDecoration(
+
+              //В зависиммости от значения 'isReady' показываем тот или иной цвет
               color: isReady ? Colors.red : Colors.black,
+
+              //Здесь тоже самое, только превращаем круг в квадрат и наоборот
               borderRadius:
                   BorderRadius.all(Radius.circular(isReady ? 0 : 50))),
+          //Размер нашего квадрата, если захотим, можем его тоже анимировать
+          //например width: isReady ? 200 : 100
           width: 100,
           height: 100,
-          duration: const Duration(milliseconds: 80),
+
+          //Сколько будет длиться анимация
+          duration: const Duration(milliseconds: 350),
+          //Это по какой кривой будет воспроизводится анимация, вариаций много
           curve: Curves.easeOutBack,
+
+          //Анимируемый алигн xD
+          //Тут все так-же, в зависиммости от значения isReady мы задаем тот или иной параметр
           child: AnimatedAlign(
               alignment: isReady ? Alignment.topCenter : Alignment.center,
               duration: const Duration(milliseconds: 150),
